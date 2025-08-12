117ab1200f072888c446f186d2367a73d9f63937
Seahyoung <seamayet@inha.edu>
2025-08-12 23:28:00 +0900

    수정test03

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -38,6 +38,9 @@ export default function MainPage() {
                 <h1 className="main-title ">ColorSync</h1>
                 <p className="sub-title text-5xl">데이터에 감각을 더하다1</p>
                 <p className="sub-title text-5xl">데이터에 감각을 더하다2</p>
+                <p className="sub-title text-5xl">
+                    데이터에 감각을 더하다3 제발좀 돼리
+                </p>
                 <button
                     onClick={() => navigate("/dashboard")}
                     className="start text-6xl"
